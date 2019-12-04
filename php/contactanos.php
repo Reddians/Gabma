f6ce6926315a3f75847951da801ce1105e743eec
snaider rodriguez <snaider.rodriguez@ias.co.com>
2019-12-04 16:32:51 -0500

    configuracion del php contactanos, mision vision y objetivos de la compiañia

--- php/contactanos.php
+++ php/contactanos.php
@@ -8,11 +8,12 @@
     //if($response->isSuccess()){
         //conexion con la base de datos
 
+
 $usuario = "root";
 $contrasena = ""; 
 $servidor = "localhost";
-$basededatos = "dbreddiansoft";
-$conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base datos");
+$basededatos = "DBReddianSoft";
+$conexion = mysqli_connect( $servidor, $usuario, $contrasena ) or die ("No se ha podido conectar al servidor de Base datos");
 $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
 
 //Obtenemos los valores del formulario
